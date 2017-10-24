8938e9d1b828f7299982756cad20ebeb52425e27
m809745357 <m809745357@gmail.com>
2017-10-24 02:59:51 +0000

    修改菜单文字颜色

--- resources/views/threads/menu.blade.php
+++ resources/views/threads/menu.blade.php
@@ -3,6 +3,6 @@
     <li><a href="{{ route('home') }}" class="menu-item"><img src="/images/home.png" alt="">首页</a></li>
     <li><a href="{{ route('categories.index') }}" class="menu-item"><img src="/images/category.png" alt="">分类</a></li>
     <li><a href="{{ route('posts.index') }}" class="menu-item post"><img src="/images/post.png" alt="">发布</a></li>
-    <li><a href="{{ route('threads.index') }}" class="menu-item"><img src="/images/thread-on.png" alt="">问答</a></li>
+    <li><a href="{{ route('threads.index') }}" class="menu-item on"><img src="/images/thread-on.png" alt="">问答</a></li>
     <li><a href="{{ route('user.index') }}" class="menu-item"><img src="/images/user.png" alt="">我的</a></li>
 </div>
--- resources/views/users/menu.blade.php
+++ resources/views/users/menu.blade.php
@@ -4,5 +4,5 @@
     <li><a href="{{ route('categories.index') }}" class="menu-item"><img src="/images/category.png" alt="">分类</a></li>
     <li><a href="{{ route('posts.index') }}" class="menu-item post"><img src="/images/post.png" alt="">发布</a></li>
     <li><a href="{{ route('threads.index') }}" class="menu-item"><img src="/images/thread.png" alt="">问答</a></li>
-    <li><a href="{{ route('user.index') }}" class="menu-item"><img src="/images/user-on.png" alt="">我的</a></li>
+    <li><a href="{{ route('user.index') }}" class="menu-item on"><img src="/images/user-on.png" alt="">我的</a></li>
 </div>
